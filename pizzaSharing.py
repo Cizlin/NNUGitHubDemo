35e2a6367776f7facf5ab21d1a87f78ac9afbc42
Cizlin <45015771+Cizlin@users.noreply.github.com>
2019-11-05 16:58:45 -0700

    Add files via upload

---
+++ pizzaSharing.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+pizzas = int(input("How many pizzas do you have? "))
+students = int(input("How many students are there? "))
+
+print(f"Each student should eat {pizzas/students} pizzas")
